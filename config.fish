55d22d0fcbe9667c34da404549cae94ab3ec0a0d
Bjoern Rochel <bjoern.rochel@xing.com>
2016-12-22 11:45:59 +0100

    Change branch color to white

--- config.fish
+++ config.fish
@@ -10,7 +10,7 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch FF8000
+set __fish_git_prompt_color_branch normal
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
