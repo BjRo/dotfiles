2662269952a3a7f8503647cb818e3dcede50924c
Bjoern Rochel <bjoern.rochel@xing.com>
2016-12-22 11:43:23 +0100

    Add fish config

---
+++ config.fish
@@ -0,0 +1,65 @@
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch FF8000
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+  set last_status $status
+
+  set_color -o black 
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
+
+# Environment variables
+set -x BUNDLER_EDITOR vim
+set -x EDITOR vim
+
+
+# Functions & aliases
+function st
+  command git status $argv
+end
+
+function be
+  command bundle exec $argv
+end
+
+function bo
+  command bundle open $argv
+end
+
+function bi
+  command bundle install $argv
+end
+
+function g
+  command git $argv
+end
+
+function tmux
+  env TERM=screen-256color-bce tmux $argv
+end
